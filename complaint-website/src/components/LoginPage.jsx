3049d21207ad9d3774037f3111217bf9db0af226
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2024-09-02 09:47:58 +0700

    fix width of card

--- complaint-website/src/components/LoginPage.jsx
+++ complaint-website/src/components/LoginPage.jsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const LoginContainer = styled.div`
-  width: 100%;
+  width: 90%;
   max-width: 400px;
   margin: 100px auto;
   padding: 20px;
   background-color: #f9f9f9;
   border: 2px solid #2f6d31;
   border-radius: 8px;
+  
 `;
 
 const Input = styled.input`
